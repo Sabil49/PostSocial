56c37596504e8edc3aa51daa290f0c9338e77012
Sabil49 <111126146+Sabil49@users.noreply.github.com>
2025-09-05 23:01:54 +0530

    Update page.tsx

--- src/app/SocialAccount/page.tsx
+++ src/app/SocialAccount/page.tsx
@@ -7,8 +7,7 @@
         const redirectUri = 'https://post-social-opal.vercel.app/api/auth/callback'; // Replace with your configured redirect URI
         const scopes = 'users.read tweet.read'; // Example scopes, adjust as needed
 
-        const authUrl = `https://twitter.com/i/oauth2/authorize?response_type=code&client_id=${clientId}&redirect_uri=${encodeURIComponent(redirectUri)}&scope=${encodeURIComponent(scopes)}&state=STATE_VALUE&code_challenge=CHALLENGE_VALUE&code_challenge_method=s256`;
-
+        const authUrl = `https://twitter.com/i/oauth2/authorize?response_type=code&client_id=${clientId}&redirect_uri=${encodeURIComponent(redirectUri)}&scope=${encodeURIComponent(scopes)}&state=state123&code_challenge=challenge&code_challenge_method=plain`;
         window.location.href = authUrl;
       };
 
