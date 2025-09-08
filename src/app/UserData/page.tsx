163b175e67e23097635e6dbbd062bb4943132afb
Sabil49 <md.sabeel10@gmail.com>
2025-09-08 17:01:15 +0530

    change text

--- src/app/UserData/page.tsx
+++ src/app/UserData/page.tsx
@@ -2,7 +2,7 @@ export default function UserDataPage() {
     return (
       <div>
         <h1>User Data Page</h1>
-        <p>This is a placeholder for user data display.</p>
+        <p>This is a space for user data display.</p>
       </div>
     );
   }
