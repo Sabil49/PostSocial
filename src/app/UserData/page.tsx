070be7579c2b2401496aae973db51d03a0b9c62c
Sabil49 <md.sabeel10@gmail.com>
2025-09-08 16:44:24 +0530

    add auth

---
+++ src/app/UserData/page.tsx
@@ -0,0 +1,8 @@
+export default function UserDataPage() {
+    return (
+      <div>
+        <h1>User Data Page</h1>
+        <p>This is a placeholder for user data display.</p>
+      </div>
+    );
+  }
