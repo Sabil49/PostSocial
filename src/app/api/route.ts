a45549f5a3d3d98d4eb2eb81d8085c7b47629e79
Sabil49 <md.sabeel10@gmail.com>
2025-09-08 10:28:21 +0530

    change code 11

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,47 +1,43 @@
-import { NextRequest } from 'next/server';
+import { NextRequest,NextResponse } from 'next/server';
 
 export async function GET(req: NextRequest) {
     const { searchParams } = new URL(req.url);
     const code = searchParams.get('code');  
-    //console.log('Received code:', code);
-    if (!code) {
-     //console.error('Authorization code missing from callback.');
+    if (!code) {     
         return new Response('Authorization failed', { status: 400 });
       }
-  console.error('Authorization code is present...');
-  return new Response(`code...: ${code}`, { status: 200 });
-  // try {
-  //   console.log('Authorization code received:', code);
+  
+  //return new Response(`code...: ${code}`, { status: 200 });
+   try {
     
-  //   const tokenResponse = await fetch('https://api.x.com/2/oauth2/token', {
-  //     method: 'POST',
-  //     headers: {
-  //       'Content-Type': 'application/x-www-form-urlencoded',
-  //     },
-  //     body: new URLSearchParams({
-  //       grant_type: 'authorization_code',
-  //       client_id: 'dlg5alhxWHM2V3pMcFpaSUJ3Rm46MTpjaQ',
-  //       client_secret: 'sj715FPU4w3YAASKfegihOyYXAvitAjEq2IGyIkRsQv2yKH9DL',
-  //       code: code,
-  //       redirect_uri: 'https://post-social-opal.vercel.app/api/',
-  //       // Add code_verifier if using PKCE
-  //     }).toString(),
-  //   });
+     const tokenResponse = await fetch('https://api.x.com/2/oauth2/token', {
+       method: 'POST',
+     headers: {
+         'Content-Type': 'application/x-www-form-urlencoded',
+       },
+       body: new URLSearchParams({
+         grant_type: 'authorization_code',
+         client_id: 'dlg5alhxWHM2V3pMcFpaSUJ3Rm46MTpjaQ',
+         client_secret: 'sj715FPU4w3YAASKfegihOyYXAvitAjEq2IGyIkRsQv2yKH9DL',
+         code: code,
+         redirect_uri: 'https://post-social-opal.vercel.app/api/',
+         // Add code_verifier if using PKCE
+       }).toString(),
+     });
 
-  //   const data = await tokenResponse.json();
+     const data = await tokenResponse.json();
 
-  //   if (data.error) {
-  //     return NextResponse.json({ error: tokenResponse.status }, { status: 400 });
-  //   }
+     if (data.error) {
+         return new Response(`${tokenResponse.status}`, { status: 400 });
+     }
 
-  //   const res = NextResponse.redirect(new URL('/', req.url));
-  //       res.cookies.set('accessToken', `${data.access_token}`, { httpOnly: true, secure: true });
-  //       return res;
+     const res = NextResponse.redirect(new URL('/', req.url));
+       res.cookies.set('accessToken', `${data.access_token}`, { httpOnly: true, secure: true });
+       return res;
 
-  // } catch (error) {
-  //   console.error('Token exchange error:', error);
-  //   return NextResponse.json({ error: 'Failed to exchange code for token' }, { status: 500 });
-  // }
+   } catch (error) {
+       return new Response(`Error in token exchange: ${error}`, { status: 500 });
+   }
 }
 
 
