0e0dce008bd46669ecd452e1cdf8d750f3f7312c
Sabil49 <md.sabeel10@gmail.com>
2025-09-09 14:55:45 +0530

    add gemini 2

--- src/app/Gemini/page.tsx
+++ src/app/Gemini/page.tsx
@@ -6,7 +6,7 @@ export default function GeminiPage() {
 async function main() {
   const response = await ai.models.generateContent({
     model: "gemini-2.5-flash",
-    contents: "Explain how AI works in a few words",
+    contents: "Explain how AI works in a few words.",
   });
   console.log(response.text);
 }
