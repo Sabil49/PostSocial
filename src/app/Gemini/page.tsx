678770371470231e6c4a0dbeb567961c4d9a1905
Sabil49 <md.sabeel10@gmail.com>
2025-09-09 21:58:19 +0530

    Gemini data

--- src/app/Gemini/page.tsx
+++ src/app/Gemini/page.tsx
@@ -5,7 +5,7 @@ export default async function Responsedata() {
 
        const cookieStore = await cookies();
        const Geminidata = cookieStore.get('Geminidata')?.value; // Using optional chaining for safety
-
+      console.log(cookieStore.get('twitterData')?.value);
       return (
         <div>
            {
