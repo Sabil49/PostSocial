b1cd09eeccce011da4c276e8fd32475908ba59dc
Sabil49 <md.sabeel10@gmail.com>
2025-09-09 21:13:53 +0530

    Gemini data

--- src/app/Gemini/page.tsx
+++ src/app/Gemini/page.tsx
@@ -4,12 +4,12 @@
 export default async function Responsedata() {
 
        const cookieStore = await cookies();
-       const GeminiData = cookieStore.get('GeminiData')?.value; // Using optional chaining for safety
+       const Geminidata = cookieStore.get('Geminidata')?.value; // Using optional chaining for safety
 
       return (
         <div>
            {
-            GeminiData ? <p>Gemini&apos;s Response: {GeminiData}</p> : 'Loading...'
+            Geminidata ? <p>Gemini&apos;s Response: {Geminidata}</p> : 'Loading...'
           } 
         </div>
       );
