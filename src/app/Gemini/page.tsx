f092f3c7f88ccd8a1a1db8c98c97b86c0826ef1a
Sabil49 <md.sabeel10@gmail.com>
2025-09-09 14:59:04 +0530

    add gemini 4

--- src/app/Gemini/page.tsx
+++ src/app/Gemini/page.tsx
@@ -2,15 +2,13 @@ import { GoogleGenAI } from "@google/genai";
 
 const ai = new GoogleGenAI({ apiKey: process.env.NEXT_PUBLIC_GOOGLE_GEMINI_API_KEY || "" });
 
-export default function GeminiPage() {  
-async function main() {
+export default async function GeminiPage() {  
+
   const response = await ai.models.generateContent({
     model: "gemini-2.5-flash",
     contents: "Explain how AI works in a few words.",
   });
   console.log(response.text);
   return <div>{response.text}</div>;
-}
 
-main();
 }
