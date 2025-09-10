14eb5eb597804b7031e9648d5a7a32bb4e3abafb
Sabil49 <md.sabeel10@gmail.com>
2025-09-10 18:05:07 +0530

    change to x

--- src/app/api/Xdata/route.ts
+++ src/app/api/Xdata/route.ts
@@ -13,29 +13,30 @@ export async function GET(req: NextRequest) {
     },
             });
        const userData = await response.json();
-       if (!userData.data.id) {
-        return new Response(JSON.stringify({ error: 'User ID not found' }), { status: 401 });
-       }
-       const twitterResponse = await fetch(`https://api.twitter.com/2/users/${userData.data.id}/tweets`, {
-      headers: {
-        Authorization: `Bearer ${accessToken}`,
-      },
-    });
+       return NextResponse.json(userData);
+    //    if (!userData.data.id) {
+    //     return new Response(JSON.stringify({ error: 'User ID not found' }), { status: 401 });
+    //    }
+    //    const twitterResponse = await fetch(`https://api.twitter.com/2/users/${userData.data.id}/tweets`, {
+    //   headers: {
+    //     Authorization: `Bearer ${accessToken}`,
+    //   },
+    // });
 
-       const tweetData = await twitterResponse.json();
+    //    const tweetData = await twitterResponse.json();
 
-       const GeminiResponse = await ai.models.generateContent({
-           model: "gemini-2.5-flash",
-           contents: "Convert the data in row format " + JSON.stringify(tweetData),
-         });
-        // Access the generated content from GeminiResponse
-        const GeminiResponseData = GeminiResponse.candidates?.[0]?.content ?? null;
-         if (!GeminiResponseData) {
-           return new Response(JSON.stringify({ error: 'Failed to generate content' }), { status: 500 });
-         } 
-         const responseRedirect = NextResponse.redirect(new URL('/Gemini', req.url));
-         responseRedirect.cookies.set('Geminidata', JSON.stringify(GeminiResponseData), { httpOnly: true, secure: true });
-         responseRedirect.cookies.set('twitterData', JSON.stringify(tweetData), { httpOnly: true, secure: true });
+    //    const GeminiResponse = await ai.models.generateContent({
+    //        model: "gemini-2.5-flash",
+    //        contents: "Convert the data in row format " + JSON.stringify(tweetData),
+    //      });
+    //     // Access the generated content from GeminiResponse
+    //     const GeminiResponseData = GeminiResponse.candidates?.[0]?.content ?? null;
+    //      if (!GeminiResponseData) {
+    //        return new Response(JSON.stringify({ error: 'Failed to generate content' }), { status: 500 });
+    //      } 
+    //      const responseRedirect = NextResponse.redirect(new URL('/Gemini', req.url));
+    //      responseRedirect.cookies.set('Geminidata', JSON.stringify(GeminiResponseData), { httpOnly: true, secure: true });
+    //      responseRedirect.cookies.set('twitterData', JSON.stringify(tweetData), { httpOnly: true, secure: true });
 
-         return responseRedirect;
+    //      return responseRedirect;
 }
