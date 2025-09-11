b49e568bb9d1bed4c26ad7713a94a3498b99ea28
Sabil49 <md.sabeel10@gmail.com>
2025-09-11 20:31:35 +0530

    change to x

--- src/app/api/tweetdata/route.ts
+++ src/app/api/tweetdata/route.ts
@@ -47,8 +47,8 @@ export async function GET(req: NextRequest) {
          if (!GeminiResponseData) {
            return new Response(JSON.stringify({ error: 'Failed to generate content' }), { status: 500 });
          }
-          
-          const responseRedirect = NextResponse.redirect(new URL('/Gemini?data=' + JSON.stringify(GeminiResponseData), req.url));
+
+          const responseRedirect = NextResponse.redirect(new URL('/Gemini?data=' + encodeURIComponent(JSON.stringify(GeminiResponseData)), req.url));
          //responseRedirect.cookies.set('Geminidata', JSON.stringify(GeminiResponseData), { httpOnly: true, secure: true });
          //responseRedirect.cookies.set('twitterData', JSON.stringify(tweetData), { httpOnly: true, secure: true });
 
