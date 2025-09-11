fcb86989b1541946f2d1cdf118feb4814c70dd67
Sabil49 <md.sabeel10@gmail.com>
2025-09-11 13:37:55 +0530

    change to x

--- src/app/SocialAccount/page.tsx
+++ src/app/SocialAccount/page.tsx
@@ -1,23 +1,32 @@
     "use client";
     import React from 'react';
+    import { Suspense } from 'react';
     import { useSearchParams } from 'next/navigation';
 
+    function MyErrorComponent() {
+     const searchParams = useSearchParams();
+     const errorParam = searchParams.get('error');
+     const errorString = errorParam?.toString(); // Get the full query string
+     console.log('searchParams:', searchParams);
+     // ... your component logic using 'query'
+     return <div>{errorString ? <div style={{ color: 'red' }}>{errorString}</div> : null}</div>;
+    }
+
     function ConnectXButton() {
-      const searchParams = useSearchParams();
-      const errorParam = searchParams.get('error'); // Get a specific parameter
-      const errorString = errorParam?.toString(); // Get the full query string
-      console.log('searchParams:', searchParams);
       const handleConnectX = () => {
         window.location.href = process.env.NEXT_PUBLIC_X_AUTH_URL ?? '';
       };
 
       return (
-        <button onClick={handleConnectX}>
-          <b>Connect with Twitter</b>
-          {
-            errorString ? <p style={{ color: 'red' }}>{errorString}</p> : null
-          }
-        </button>
+        <div>
+          <button onClick={handleConnectX}>
+            <b>Connect with Twitter</b>
+          </button>
+          <Suspense fallback={<div>Loading error...</div>}>
+            <MyErrorComponent />
+          </Suspense>
+        </div>
+        
       );
     }
 
