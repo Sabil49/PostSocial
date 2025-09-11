d59e277f50e2b753beefbcd3c1fb9916c72720a6
Sabil49 <md.sabeel10@gmail.com>
2025-09-11 13:12:18 +0530

    change to x

--- src/app/api/Xdata/route.ts
+++ src/app/api/Xdata/route.ts
@@ -16,31 +16,31 @@ export async function GET(req: NextRequest) {
             if (userData.error) {
              return new Response(JSON.stringify({ error: 'Please try after 15 minutes' }), { status: 401 });
             }
-            //return NextResponse.json({ userData });
-       if (!userData.data || !userData.data.id) {
-        return new Response(JSON.stringify({ error: 'User ID not found' }), { status: 401 });
-       }
-       const twitterResponse = await fetch(`https://api.x.com/2/users/${userData.data.id}/tweets?max_results=5&expansions=author_id,referenced_tweets.id,attachments.media_keys&tweet.fields=created_at,public_metrics,entities,source&user.fields=username,name,profile_image_url&media.fields=url`, {
-      headers: {
-        Authorization: `Bearer ${accessToken}`,
-      },
-    });
+            return NextResponse.json({ userData });
+    //    if (!userData.data || !userData.data.id) {
+    //     return new Response(JSON.stringify({ error: 'User ID not found' }), { status: 401 });
+    //    }
+    //    const twitterResponse = await fetch(`https://api.x.com/2/users/${userData.data.id}/tweets?max_results=5&expansions=author_id,referenced_tweets.id,attachments.media_keys&tweet.fields=created_at,public_metrics,entities,source&user.fields=username,name,profile_image_url&media.fields=url`, {
+    //   headers: {
+    //     Authorization: `Bearer ${accessToken}`,
+    //   },
+    // });
 
-       const tweetData = await twitterResponse.json();
-        //return NextResponse.json(tweetData);
-       const GeminiResponse = await ai.models.generateContent({
-           model: "gemini-2.5-flash",
-           contents: "Do sentiment analysis for tweet. use 'Text' field as tweet from provided data. return result in separate field as 'sentiment'." + JSON.stringify(tweetData),
-         });
-        // Access the generated content from GeminiResponse
-        const GeminiResponseData = GeminiResponse.text;
-         if (!GeminiResponseData) {
-           return new Response(JSON.stringify({ error: 'Failed to generate content' }), { status: 500 });
-         } 
-         //return NextResponse.json({ GeminiResponseData });
-         const responseRedirect = NextResponse.redirect(new URL('/Gemini?data=' + encodeURIComponent(JSON.stringify(GeminiResponseData)), req.url));
-         //responseRedirect.cookies.set('Geminidata', JSON.stringify(GeminiResponseData), { httpOnly: true, secure: true });
-         //responseRedirect.cookies.set('twitterData', JSON.stringify(tweetData), { httpOnly: true, secure: true });
+    //    const tweetData = await twitterResponse.json();
+    //     //return NextResponse.json(tweetData);
+    //    const GeminiResponse = await ai.models.generateContent({
+    //        model: "gemini-2.5-flash",
+    //        contents: "Do sentiment analysis for tweet. use 'Text' field as tweet from provided data. return result in separate field as 'sentiment'." + JSON.stringify(tweetData),
+    //      });
+    //     // Access the generated content from GeminiResponse
+    //     const GeminiResponseData = GeminiResponse.text;
+    //      if (!GeminiResponseData) {
+    //        return new Response(JSON.stringify({ error: 'Failed to generate content' }), { status: 500 });
+    //      } 
+    //      //return NextResponse.json({ GeminiResponseData });
+    //      const responseRedirect = NextResponse.redirect(new URL('/Gemini?data=' + encodeURIComponent(JSON.stringify(GeminiResponseData)), req.url));
+    //      //responseRedirect.cookies.set('Geminidata', JSON.stringify(GeminiResponseData), { httpOnly: true, secure: true });
+    //      //responseRedirect.cookies.set('twitterData', JSON.stringify(tweetData), { httpOnly: true, secure: true });
 
-         return responseRedirect;
+    //      return responseRedirect;
 }
