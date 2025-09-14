f72727528e468c9e6b83aa2c0c7ebcec54b89e5c
Sabil49 <md.sabeel10@gmail.com>
2025-09-14 18:19:21 +0530

    add new prisma

---
+++ app/Testdata/page.tsx
@@ -0,0 +1,21 @@
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+export default async function Testdata() {
+  const users = await prisma.user.findMany();
+  return (
+    <div className="min-h-screen bg-gray-50 flex flex-col items-center justify-center -mt-16">
+      <h1 className="text-4xl font-bold mb-8 font-[family-name:var(--font-geist-sans)] text-[#333333]">
+        Superblog
+      </h1>
+      <ol className="list-decimal list-inside font-[family-name:var(--font-geist-sans)]">
+        {users.map((user: { id: string; name: string | null }) => (
+          <li key={user.id} className="mb-2">
+            {user.name}
+          </li>
+        ))}
+      </ol>
+    </div>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import ConnectXButton from "./SocialAccount/page";
+import Testdata from "./Testdata/page";
 
 export default function Home() {
   return (
     <div className="">
       <ConnectXButton />
+      <Testdata />
     </div>
   );
 }
