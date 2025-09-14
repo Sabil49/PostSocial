e8131e53b1dd78fbca1f7796bb05c3f6979675f3
Sabil49 <md.sabeel10@gmail.com>
2025-09-14 17:51:51 +0530

    add new prisma

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,9 @@
 import ConnectXButton from "./SocialAccount/page";
-import Testdata from "./Testdata/page";
 
 export default function Home() {
   return (
     <div className="">
       <ConnectXButton />
-      <Testdata />
     </div>
   );
 }
