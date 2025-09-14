6792cecb367a150a158880d48f8a239280db1d42
Sabil49 <md.sabeel10@gmail.com>
2025-09-14 17:03:12 +0530

    add new prisma

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from '../../app/generated/prisma'
+import { PrismaClient } from '../app/generated/prisma'
 import { withAccelerate } from '@prisma/extension-accelerate'
 
 const globalForPrisma = global as unknown as { 
@@ -9,4 +9,4 @@ const prisma = globalForPrisma.prisma || new PrismaClient().$extends(withAcceler
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
 
-export default prisma;
+export default prisma
