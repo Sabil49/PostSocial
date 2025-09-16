440de252dbd7215f05879b46bf323a1b2be10b18
Sabil49 <md.sabeel10@gmail.com>
2025-09-16 13:18:17 +0530

    add token

--- app/SocialAccount/page.tsx
+++ app/SocialAccount/page.tsx
@@ -19,11 +19,13 @@
       const { data: session } = useSession();
       if (session) {
         return (
+          <><pre>{JSON.stringify(session, null, 2)}</pre>
           <form action={signOutUser}>
             <button className="flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3">
               <div className="hidden md:block">Sign Out</div>
             </button>
           </form>
+          </>
         );
       }
         
