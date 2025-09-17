77c2638f27fc6d8994c82018c7deb241181b62ef
Sabil49 <md.sabeel10@gmail.com>
2025-09-17 16:26:57 +0530

    add css

--- app/Gemini/page.tsx
+++ app/Gemini/page.tsx
@@ -58,8 +58,8 @@ import clientData from '@/utils/clientData.json';
 export default function Responsedata() {
 
     return (
-        <div className=' max-w-4xl mx-auto p-4 m-[10px_auto] border bg-white shadow-md'>
-          <div id="json-output" className='grid grid-cols-2 gap-4 border p-2.5 rounded-md'></div>
+        <div className=' max-w-[90%] mx-auto p-4 m-[10px_auto] bg-white shadow-md'>
+          <div id="json-output" className='grid grid-cols-2 gap-4 *:border *:p-2.5 *:rounded-md'></div>
           <Suspense fallback={<div>Loading error...</div>}>
                       <MyGeminiComponent />
           </Suspense>
