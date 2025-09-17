2f237da5eeb1fc21201e37a7fee9e82550c4c131
Sabil49 <md.sabeel10@gmail.com>
2025-09-17 16:13:56 +0530

    add css

--- app/Gemini/page.tsx
+++ app/Gemini/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useSearchParams } from 'next/navigation';
+//import { useSearchParams } from 'next/navigation';
 import { Suspense } from 'react';
 import clientData from '@/utils/clientData.json';
 
