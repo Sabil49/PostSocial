80854527d8881bafd2d792a9a6c670e0fef89d00
Sabil49 <md.sabeel10@gmail.com>
2025-09-26 21:00:14 +0530

    check heatmap

--- app/SocialAccount/page.tsx
+++ app/SocialAccount/page.tsx
@@ -4,7 +4,7 @@
     import { useSearchParams } from 'next/navigation';
     import {signOutUser,} from '@/app/api/auth/actions';
     import { useSession } from "next-auth/react";
-   import TweetWordCloud from '../Charts/Word cloud/page';
+   import TweetWordCloud from '../Charts/WordCloud/page';
     
     function MyErrorComponent() {
      const searchParams = useSearchParams();
