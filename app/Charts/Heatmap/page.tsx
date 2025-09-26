680aca77a85e2ad2f7a40474bb2b4b2da499eace
Sabil49 <md.sabeel10@gmail.com>
2025-09-26 20:02:38 +0530

    check heatmap

--- app/Charts/Heatmap/page.tsx
+++ app/Charts/Heatmap/page.tsx
@@ -7,9 +7,11 @@ export default function BasicHeatmap() {
   return (
     <Box sx={{ width: '100%', maxWidth: 400 }}>
       <Heatmap
-        xAxis={[{ data: [1, 2, 3, 4] }]}
-        yAxis={[{ data: ['A', 'B', 'C', 'D', 'E'] }]}
-        series={[{ data }]}
+          xAxis={[{ data: ['likes', 'replies', 'retweets'], scaleType: "band" }]}
+          yAxis={[{ data: ["Engagement"], scaleType: "band" }]}
+        series={[{  data: [
+          [11.48, 0.65, 0.52], // values aligned with xAxis
+        ], }]}
         height={310}
       />
     </Box>
