2aabaaef78b5186e2b696b99490fb67d995cee33
Sabil49 <md.sabeel10@gmail.com>
2025-09-28 08:17:26 +0530

    db expand

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
 import LoginPage from "./login/page";
-import Testdata from "./Testdata/page";
+// import Testdata from "./Testdata/page";
 
 export default function Home() {
   return (
     <div className="">
       <LoginPage />
-      <Testdata />
+      {/* <Testdata /> */}
     </div>
   );
 }
