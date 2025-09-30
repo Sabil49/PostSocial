5712408d40dbe766180f7231cb92e141606d1515
Sabil49 <md.sabeel10@gmail.com>
2025-09-30 07:54:57 +0530

    dodo test payment

--- app/Subscription/page.tsx
+++ app/Subscription/page.tsx
@@ -1,14 +1,18 @@
 "use client";
+import { useSession } from "next-auth/react";
 import CustomerPortal from "./customer-portal";
 
+
 export default function SubscribeButton() {
+  const { data: session } = useSession();
+  const email= session?.user?.email || "test@example.com";
   const subscribe = async () => {
     const resp = await fetch("/api/payments/dodo/checkout", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
         planId: "pdt_ctSjb2435t8p2c1vQcx98",         // plan created in Dodo dashboard
-        customerEmail: "test@example.com",    // get customer email from your auth/session system
+        customerEmail: email,
       }),
     });
 
@@ -21,13 +25,17 @@ export default function SubscribeButton() {
 
   return (
     <div>
-      <button
-        onClick={subscribe}
-        className="px-4 py-2 bg-blue-600 text-white rounded"
-      >
-        Subscribe Now
-      </button>      
-      <CustomerPortal />
+      {!session && (
+        <>
+        <button
+          onClick={subscribe}
+          className="px-4 py-2 bg-blue-600 text-white rounded"
+        >
+          Subscribe Now
+        </button>
+        <CustomerPortal />
+        </>
+      )}      
     </div>
   );
 }
