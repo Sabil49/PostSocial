08c14609a8a3f3ccf64e80612daa8c43331c55dd
Sabil49 <md.sabeel10@gmail.com>
2025-09-30 17:01:11 +0530

    dodo test payment

--- app/Subscription/customer-portal.tsx
+++ app/Subscription/customer-portal.tsx
@@ -1,7 +1,14 @@
 "use client";
 export default function CustomerPortal() {
     const openPortal = async () => {
-  const res = await fetch("/api/payments/dodo/customer-portal?customer_id=cus_12345");
+  const res = await fetch("/api/payments/dodo/customer-portal?customer_id=cus_12345",{
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      "Authorization": `Bearer ${process.env.DODO_PAYMENTS_API_KEY}`,
+      "Access-Control-Allow-Origin": "*",
+    },
+  });
   const data = await res.json();
   if (data.portal_url) {
     window.location.href = data.portal_url;
