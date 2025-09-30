b7b5762840b186c8a10560845b03147f3b902514
Sabil49 <md.sabeel10@gmail.com>
2025-09-30 18:23:15 +0530

    dodo test payment

--- app/Subscription/status/success/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function StatusPage() {
-  return (
-    <div>
-      <h1>Subscription Status</h1>
-      <p className=" text-green-600 font-bold">Your subscription is active.</p>
-    </div>
-  );
-}
