a671883a8e4f409a8e820bfa05d0780323e9b8a2
Sabil49 <md.sabeel10@gmail.com>
2025-10-07 21:36:51 +0530

    subscription error handling

--- app/api/payment/dodo/checkout/route.ts
+++ app/api/payment/dodo/checkout/route.ts
@@ -1,30 +1,3 @@
-// app/api/checkout/route.ts
-// import { NextResponse } from "next/server";
-// import DodoPayments from "dodopayments";
-
-// const client = new DodoPayments({
-//   bearerToken: process.env.DODO_PAYMENTS_API_KEY
-// });
-
-// export async function POST(req: Request) {
-//   try {
-//     const { planId, customerEmail, name } = await req.json();
-//     // Create a subscription checkout session
-//     const checkoutSessionResponse = await client.checkoutSessions.create({
-//   product_cart: [{ product_id: planId, quantity: 1 }],
-//   customer: { email: customerEmail , name: name },
-//   return_url: process.env.DODO_PAYMENTS_RETURN_URL,
-// });
-
-//     return NextResponse.json({ checkout_url: checkoutSessionResponse });
-//   } catch (error) {
-//     if (error instanceof Error) {
-//       return NextResponse.json({ error: error.message }, { status: 500 });
-//     }
-//     return NextResponse.json({ error: "An unknown error occurred" }, { status: 500 });
-//   }
-// }
-
 import { Checkout } from "@dodopayments/nextjs";
 import { NextRequest } from "next/server";
 
